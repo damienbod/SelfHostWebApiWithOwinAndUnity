5e863c3c9cd647698c79aae6f15b4ea7e4feec78
damienbod <damien_bod@hotmail.com>
2013-09-24 21:28:37 +0200

    clean up

--- SelfHostWebApiOwin/Properties/AssemblyInfo.cs
+++ SelfHostWebApiOwin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SelfHostWebApiOwin")]
+[assembly: AssemblyTitle("SelfHostWebApiOwin.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SelfHostWebApiOwin")]
+[assembly: AssemblyProduct("SelfHostWebApiOwin.Web")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1465b2cc-3399-4c35-8156-a82d341fd15a")]
+[assembly: Guid("a432800a-dafd-49e8-bdc8-fc1f34033f92")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
